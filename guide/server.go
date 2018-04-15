6651f154e50f4aad8460dfbeddcb67906add7108
xiaogui-2017 <923445457@qq.com>
2018-04-16 01:10:00 +0800

    guide 完成

--- guide/server.go
+++ guide/server.go
@@ -1,12 +1,17 @@
-package guide
+package main
 
 import (
 	"net/http"
-
 	"github.com/labstack/echo"
 	"os"
 	"io"
+	"github.com/labstack/echo/middleware"
 )
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
 
 // 获取路径参数    /usres/id中的参数id，并打印出来
 func getUser(c echo.Context) error {
@@ -22,6 +27,7 @@ func show(c echo.Context) error {
 	return c.String(http.StatusOK, "team:"+team+", member:"+member)
 }
 
+//x-www-form-urlencoded 参数格式
 func save_xxx_form(c echo.Context) error {
 	// Get name and email
 	name := c.FormValue("name")
@@ -29,10 +35,9 @@ func save_xxx_form(c echo.Context) error {
 	return c.String(http.StatusOK, "name:"+name+", email:"+email)
 }
 
+//form-data  参数格式   表单里面有文件提交的时候
 func save(c echo.Context) error {
-	// Get name
 	name := c.FormValue("name")
-	// Get avatar
 	avatar, err := c.FormFile("avatar")
 	if err != nil {
 		return err
@@ -51,13 +56,11 @@ func save(c echo.Context) error {
 		return err
 	}
 	defer dst.Close()
-
 	// Copy
 	if _, err = io.Copy(dst, src); err != nil {
 		return err
 	}
-
-	return c.HTML(http.StatusOK, "<b>Thank you! " + name + "</b>")
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
 }
 
 func main() {
@@ -67,9 +70,45 @@ func main() {
 	})
 	e.GET("/users/:id", getUser)
 	e.GET("show", show)
-	e.POST("/save", save_xxx_form)
-	//e.POST("/users", saveUser)
+	e.POST("/save_xxx_from", save_xxx_form)
+	e.POST("save", save)
+
+	//Handling Request
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User) // 相当于实例化对象
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
+	e.Static("/static", "static")
 	//e.PUT("/users/:id", updateUser)
 	//e.DELETE("/users/:id", deleteUser)
 	e.Logger.Fatal(e.Start(":1323"))
+	// root级别
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// 组级别
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "joe" && password == "secret" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+
+	// Route level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			println("request to /users")
+			return next(c)
+		}
+	}
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	}, track)
 }
