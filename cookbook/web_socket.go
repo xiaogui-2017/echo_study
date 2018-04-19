2e2779d56075bc74c1a4b28782f444750f31517a
zhangzhiyuan <923445457@qq.com>
2018-04-19 16:44:22 +0800

    websocket

---
+++ cookbook/web_socket.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"github.com/gorilla/websocket"
+	//TODO 安装此包
+	"golang.org/x/net/websocket"
+)
+
+func hello(c echo.Context) error {
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		for {
+			// Write
+			err := websocket.Message.Send(ws, "Hello, Client!")
+			if err != nil {
+				c.Logger().Error(err)
+			}
+
+			// Read
+			msg := ""
+			err = websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+			fmt.Printf("%s\n", msg)
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
